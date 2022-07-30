838e1d8940e6a8cb0e6fc7584043cabb5c4017bb
Dion <alun.itn@gmail.com>
2022-07-30 21:48:27 +0700

    style : prettier

--- index.js
+++ index.js
@@ -1,62 +1,68 @@
-  
-var express = require('express');
+var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 var path = require("path");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
-var fetch = require('node-fetch');
-var cheerio = require('cheerio');
-app.get('/:version/:query', function(req, res) {
-    let {
-        version,
-        query
-    } = req.params;
-    // let url = `http://alkitab.mobi/${version}/${book}/${chapter}`;
-    let url = `http://alkitab.mobi/${version}/passage/${query}`;
+var fetch = require("node-fetch");
+var cheerio = require("cheerio");
+app.get("/:version/:query", function (req, res) {
+	let { version, query } = req.params;
+	// let url = `http://alkitab.mobi/${version}/${book}/${chapter}`;
+	let url = `http://alkitab.mobi/${version}/passage/${query}`;
 
-    fetch(url)
-        .then(response => response.text())
-        .then(body => {
-            let $ = cheerio.load(body);
-            let items = [];
-            $('p').filter((i, el) => {
-                let data = $(el);
-                let content = data.find('[data-begin]').first().text();
-                let title = data.find('.paragraphtitle').first().text();
-                let verse = data.find('.reftext').children().first().text().split(':').pop();
-                let type = null;
+	fetch(url)
+		.then((response) => response.text())
+		.then((body) => {
+			let $ = cheerio.load(body);
+			let items = [];
+			$("p").filter((i, el) => {
+				let data = $(el);
+				let content = data.find("[data-begin]").first().text();
+				let title = data.find(".paragraphtitle").first().text();
+				let verse = data
+					.find(".reftext")
+					.children()
+					.first()
+					.text()
+					.split(":")
+					.pop();
+				let type = null;
 
-                if (!title && !content) {
-                    data.find('.reftext').remove();
-                    content = data.text();
-                }
+				if (!title && !content) {
+					data.find(".reftext").remove();
+					content = data.text();
+				}
 
-                if (title) {
-                    type = 'title';
-                    content = title;
-                } else if (content) {
-                    type = 'content';
-                }
+				if (title) {
+					type = "title";
+					content = title;
+				} else if (content) {
+					type = "content";
+				}
 
-                if (data.attr('hidden') === 'hidden' || data.hasClass('loading') || data.hasClass('error')) {
-                    type = null;
-                }
-            
-                if (type)
-                    items.push({
-                        content,
-                        type,
-                        verse
-                    });
-            });
-            res.json({err_code : "0", response : items, messages : "Success"});
-            //response.Success(res, items, "Success Get Alkitab");
-            // res.json(items);
-        });
+				if (
+					data.attr("hidden") === "hidden" ||
+					data.hasClass("loading") ||
+					data.hasClass("error")
+				) {
+					type = null;
+				}
+
+				if (type)
+					items.push({
+						content,
+						type,
+						verse,
+					});
+			});
+			res.json({ err_code: "0", response: items, messages: "Success" });
+			//response.Success(res, items, "Success Get Alkitab");
+			// res.json(items);
+		});
 });
 
 app.listen(port);
-console.log('[Message]' + " Server Running :" + port);
+console.log("[Message]" + " Server Running :" + port);

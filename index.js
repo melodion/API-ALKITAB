c759fc63ca79a0eedb6b4385c2e8bbb11390fa2e
dion <alun.itn@gmail.com>
2025-09-19 16:19:53 +0700

    fix : update data & save final database

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const logger = createLogger({
 		new transports.File({
 			filename: "./logs/logs.log",
 			json: false,
-			maxsize: 5242880,
-			maxFiles: 5,
+			maxsize: 7242880,
+			maxFiles: 50,
 		}),
 		new transports.Console(),
 	],
@@ -320,7 +320,7 @@ app.get("/sample", function (req, res) {
 		},
 		{
 			"label": "Filipi",
-			"kitab": "Fil",
+			"kitab": "Flp",
 			"pasal": 4,
 			"kategory": "PB"
 		},
@@ -433,7 +433,7 @@ app.get("/sample", function (req, res) {
 		var label = i["label"];
 		var arr = arrayRange(1, pasal, 1);
 		arr.forEach(async (j) => {
-			let url = "http://alkitab.mobi/tb/" + kitab + "/" + j;
+			let url = "http://alkitab.mobi/net/" + kitab + "/" + j;
 			//logger.info("Kitab : " + kitab + ", Pasal : " + j);
 
 			await fetch(url)
@@ -493,66 +493,11 @@ app.get("/sample", function (req, res) {
 					// res.json(items);
 				})
 				.catch((error) => {
-					logger.error(error);
+					logger.error("Error : " + label + " Pasal : " + pasal + error);
 				});
 		});
 		//l; //ogger.info("============== End Kitab : " + kitab + " ==================");
 	});
-});
-app.get("/:version/:book/:chapter", function (req, res) {
-	let { version, book, chapter } = req.params;
-	let items = [];
-	//let url = `http://alkitab.mobi/${version}/passage/${query}`;
-	for (var i = 0; i <= 50; i++) {
-		let url = `http://alkitab.mobi/${version}/${book}/` + i;
-		console.log(url);
-		fetch(url)
-			.then((response) => response.text())
-			.then((body) => {
-				let $ = cheerio.load(body);
-
-				$("p").filter((i, el) => {
-					let data = $(el);
-					let content = data.find("[data-begin]").first().text();
-					let title = data.find(".paragraphtitle").first().text();
-					let verse = data
-						.find(".reftext")
-						.children()
-						.first()
-						.text()
-						.split(":")
-						.pop();
-					let type = null;
-
-					if (!title && !content) {
-						data.find(".reftext").remove();
-						content = data.text();
-					}
-
-					if (title) {
-						type = "title";
-						content = title;
-					} else if (content && verse != "") {
-						type = "content";
-					}
-
-					if (
-						data.attr("hidden") === "hidden" ||
-						data.hasClass("loading") ||
-						data.hasClass("error")
-					) {
-						type = null;
-					}
-					if (type) {
-						items.push({ content, type, verse });
-					}
-					logger.info("Pasal : " + i + ",Ayat : " + verse);
-				});
-
-				// res.json(items);
-			});
-	}
-	//res.json({ err_code: "0", response: items, messages: "Success" });
 });
 
 app.listen(port);

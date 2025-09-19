53dd94f0332c13542c23f648444cbf0f4c223dd9
dion <alun.itn@gmail.com>
2025-09-19 11:56:55 +0700

    fix  : query format & add data alkitab

--- index.js
+++ index.js
@@ -25,59 +25,401 @@ const logger = createLogger({
 app.get("/sample", function (req, res) {
 	let data = [
 		{
-			label: "Kejadian",
-			kitab: "Kej",
-			pasal: 50,
-			kategory: "PL",
+			"label": "Kejadian",
+			"kitab": "Kej",
+			"pasal": 50,
+			"kategory": "PL"
 		},
 		{
-			label: "Keluaran",
-			kitab: "Kel",
-			pasal: 40,
-			kategory: "PL",
+			"label": "Keluaran",
+			"kitab": "Kel",
+			"pasal": 40,
+			"kategory": "PL"
 		},
 		{
-			label: "Imamat",
-			kitab: "Ima",
-			pasal: 27,
-			kategory: "PL",
+			"label": "Imamat",
+			"kitab": "Ima",
+			"pasal": 27,
+			"kategory": "PL"
 		},
 		{
-			label: "Bilangan",
-			kitab: "Bil",
-			pasal: 36,
-			kategory: "PL",
+			"label": "Bilangan",
+			"kitab": "Bil",
+			"pasal": 36,
+			"kategory": "PL"
 		},
 		{
-			label: "Ulangan",
-			kitab: "Ula",
-			pasal: 34,
-			kategory: "PL",
+			"label": "Ulangan",
+			"kitab": "Ula",
+			"pasal": 34,
+			"kategory": "PL"
 		},
 		{
-			label: "Yosua",
-			kitab: "Yos",
-			pasal: 24,
-			kategory: "PL",
+			"label": "Yosua",
+			"kitab": "Yos",
+			"pasal": 24,
+			"kategory": "PL"
 		},
 		{
-			label: "Hakim-Hakim",
-			kitab: "Hak",
-			pasal: 21,
-			kategory: "PL",
+			"label": "Hakim-hakim",
+			"kitab": "Hak",
+			"pasal": 21,
+			"kategory": "PL"
 		},
 		{
-			label: "Rut",
-			kitab: "Rut",
-			pasal: 4,
-			kategory: "PL",
+			"label": "Rut",
+			"kitab": "Rut",
+			"pasal": 4,
+			"kategory": "PL"
 		},
 		{
-			label: "Bilangan",
-			kitab: "Ima",
-			pasal: 36,
-			kategory: "PL",
+			"label": "1 Samuel",
+			"kitab": "1Sa",
+			"pasal": 31,
+			"kategory": "PL"
 		},
+		{
+			"label": "2 Samuel",
+			"kitab": "2Sa",
+			"pasal": 24,
+			"kategory": "PL"
+		},
+		{
+			"label": "1 Raja‑raja",
+			"kitab": "1Ra",
+			"pasal": 22,
+			"kategory": "PL"
+		},
+		{
+			"label": "2 Raja‑raja",
+			"kitab": "2Ra",
+			"pasal": 25,
+			"kategory": "PL"
+		},
+		{
+			"label": "1 Tawarikh",
+			"kitab": "1Ta",
+			"pasal": 29,
+			"kategory": "PL"
+		},
+		{
+			"label": "2 Tawarikh",
+			"kitab": "2Ta",
+			"pasal": 36,
+			"kategory": "PL"
+		},
+		{
+			"label": "Ezra",
+			"kitab": "Ezr",
+			"pasal": 10,
+			"kategory": "PL"
+		},
+		{
+			"label": "Nehemia",
+			"kitab": "Neh",
+			"pasal": 13,
+			"kategory": "PL"
+		},
+		{
+			"label": "Ester",
+			"kitab": "Est",
+			"pasal": 10,
+			"kategory": "PL"
+		},
+		{
+			"label": "Ayub",
+			"kitab": "Ayb",
+			"pasal": 42,
+			"kategory": "PL"
+		},
+		{
+			"label": "Mazmur",
+			"kitab": "Mzm",
+			"pasal": 150,
+			"kategory": "PL"
+		},
+		{
+			"label": "Amsal",
+			"kitab": "Ams",
+			"pasal": 31,
+			"kategory": "PL"
+		},
+		{
+			"label": "Pengkhotbah",
+			"kitab": "Pkh",
+			"pasal": 12,
+			"kategory": "PL"
+		},
+		{
+			"label": "Kidung Agung",
+			"kitab": "Kid",
+			"pasal": 8,
+			"kategory": "PL"
+		},
+		{
+			"label": "Yesaya",
+			"kitab": "Yes",
+			"pasal": 66,
+			"kategory": "PL"
+		},
+		{
+			"label": "Yeremia",
+			"kitab": "Yer",
+			"pasal": 52,
+			"kategory": "PL"
+		},
+		{
+			"label": "Ratapan",
+			"kitab": "Rat",
+			"pasal": 5,
+			"kategory": "PL"
+		},
+		{
+			"label": "Yehezkiel",
+			"kitab": "Yeh",
+			"pasal": 48,
+			"kategory": "PL"
+		},
+		{
+			"label": "Daniel",
+			"kitab": "Dan",
+			"pasal": 12,
+			"kategory": "PL"
+		},
+		{
+			"label": "Hosea",
+			"kitab": "Hos",
+			"pasal": 14,
+			"kategory": "PL"
+		},
+		{
+			"label": "Yoel",
+			"kitab": "Yoe",
+			"pasal": 3,
+			"kategory": "PL"
+		},
+		{
+			"label": "Amos",
+			"kitab": "Amo",
+			"pasal": 9,
+			"kategory": "PL"
+		},
+		{
+			"label": "Obaja",
+			"kitab": "Oba",
+			"pasal": 1,
+			"kategory": "PL"
+		},
+		{
+			"label": "Yunus",
+			"kitab": "Yun",
+			"pasal": 4,
+			"kategory": "PL"
+		},
+		{
+			"label": "Mikha",
+			"kitab": "Mik",
+			"pasal": 7,
+			"kategory": "PL"
+		},
+		{
+			"label": "Nahum",
+			"kitab": "Nah",
+			"pasal": 3,
+			"kategory": "PL"
+		},
+		{
+			"label": "Habakuk",
+			"kitab": "Hab",
+			"pasal": 3,
+			"kategory": "PL"
+		},
+		{
+			"label": "Zefanya",
+			"kitab": "Zef",
+			"pasal": 3,
+			"kategory": "PL"
+		},
+		{
+			"label": "Hagai",
+			"kitab": "Hag",
+			"pasal": 2,
+			"kategory": "PL"
+		},
+		{
+			"label": "Zakharia",
+			"kitab": "Zak",
+			"pasal": 14,
+			"kategory": "PL"
+		},
+		{
+			"label": "Maleakhi",
+			"kitab": "Mal",
+			"pasal": 4,
+			"kategory": "PL"
+		},
+		{
+			"label": "Matius",
+			"kitab": "Mat",
+			"pasal": 28,
+			"kategory": "PB"
+		},
+		{
+			"label": "Markus",
+			"kitab": "Mar",
+			"pasal": 16,
+			"kategory": "PB"
+		},
+		{
+			"label": "Lukas",
+			"kitab": "Luk",
+			"pasal": 24,
+			"kategory": "PB"
+		},
+		{
+			"label": "Yohanes",
+			"kitab": "Yoh",
+			"pasal": 21,
+			"kategory": "PB"
+		},
+		{
+			"label": "Kisah Para Rasul",
+			"kitab": "Kis",
+			"pasal": 28,
+			"kategory": "PB"
+		},
+		{
+			"label": "Roma",
+			"kitab": "Rom",
+			"pasal": 16,
+			"kategory": "PB"
+		},
+		{
+			"label": "1 Korintus",
+			"kitab": "1Ko",
+			"pasal": 16,
+			"kategory": "PB"
+		},
+		{
+			"label": "2 Korintus",
+			"kitab": "2Ko",
+			"pasal": 13,
+			"kategory": "PB"
+		},
+		{
+			"label": "Galatia",
+			"kitab": "Gal",
+			"pasal": 6,
+			"kategory": "PB"
+		},
+		{
+			"label": "Efesus",
+			"kitab": "Efe",
+			"pasal": 6,
+			"kategory": "PB"
+		},
+		{
+			"label": "Filipi",
+			"kitab": "Fil",
+			"pasal": 4,
+			"kategory": "PB"
+		},
+		{
+			"label": "Kolose",
+			"kitab": "Kol",
+			"pasal": 4,
+			"kategory": "PB"
+		},
+		{
+			"label": "1 Tesalonika",
+			"kitab": "1Tes",
+			"pasal": 5,
+			"kategory": "PB"
+		},
+		{
+			"label": "2 Tesalonika",
+			"kitab": "2Tes",
+			"pasal": 3,
+			"kategory": "PB"
+		},
+		{
+			"label": "1 Timotius",
+			"kitab": "1Tim",
+			"pasal": 6,
+			"kategory": "PB"
+		},
+		{
+			"label": "2 Timotius",
+			"kitab": "2Tim",
+			"pasal": 4,
+			"kategory": "PB"
+		},
+		{
+			"label": "Titus",
+			"kitab": "Tit",
+			"pasal": 3,
+			"kategory": "PB"
+		},
+		{
+			"label": "Filemon",
+			"kitab": "Flm",
+			"pasal": 1,
+			"kategory": "PB"
+		},
+		{
+			"label": "Ibrani",
+			"kitab": "Ibr",
+			"pasal": 13,
+			"kategory": "PB"
+		},
+		{
+			"label": "Yakobus",
+			"kitab": "Yak",
+			"pasal": 5,
+			"kategory": "PB"
+		},
+		{
+			"label": "1 Petrus",
+			"kitab": "1Pet",
+			"pasal": 5,
+			"kategory": "PB"
+		},
+		{
+			"label": "2 Petrus",
+			"kitab": "2Pet",
+			"pasal": 3,
+			"kategory": "PB"
+		},
+		{
+			"label": "1 Yohanes",
+			"kitab": "1Yoh",
+			"pasal": 5,
+			"kategory": "PB"
+		},
+		{
+			"label": "2 Yohanes",
+			"kitab": "2Yoh",
+			"pasal": 1,
+			"kategory": "PB"
+		},
+		{
+			"label": "3 Yohanes",
+			"kitab": "3Yoh",
+			"pasal": 1,
+			"kategory": "PB"
+		},
+		{
+			"label": "Yudas",
+			"kitab": "Yud",
+			"pasal": 1,
+			"kategory": "PB"
+		},
+		{
+			"label": "Wahyu",
+			"kitab": "Why",
+			"pasal": 22,
+			"kategory": "PB"
+		}
 	];
 	const arrayRange = (start, stop, step) =>
 		Array.from(
@@ -88,6 +430,7 @@ app.get("/sample", function (req, res) {
 		var kategory = i["kategory"];
 		var kitab = i["kitab"];
 		var pasal = i["pasal"];
+		var label = i["label"];
 		var arr = arrayRange(1, pasal, 1);
 		arr.forEach(async (j) => {
 			let url = "http://alkitab.mobi/tb/" + kitab + "/" + j;
@@ -96,7 +439,7 @@ app.get("/sample", function (req, res) {
 			await fetch(url)
 				.then((response) => {
 					logger.info(
-						"Kitab :" +
+						"-- Kitab :" +
 							kitab +
 							", Pasal : " +
 							j +
@@ -143,7 +486,7 @@ app.get("/sample", function (req, res) {
 						}
 						if (type) {
 							items.push({ content, type, verse });
-							log += `INSERT INTO tb_alkitab_id(kategory, kitab, pasal, ayat, firman, flag) values ('${kategory}','${kitab}','${j}','${verse}','${content}','${type}')\n`;
+							log += `INSERT INTO tb_alkitab_id(kategory, kitab, label, pasal, ayat, firman, flag) values ('${kategory}','${kitab}','${label}','${j}','${verse}','${content}','${type}');\n`;
 						}
 					});
 					logger.info(log);
